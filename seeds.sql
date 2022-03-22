08fced2416a5eb6f857559550045f72cc42c4dd4
Trevor L <trevorlong992@gmail.com>
2022-03-22 13:22:56 -0400

    Correcting previous push

---
+++ seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO department (name)
+VALUES
+    ("Engineering"),
+    ("Finance"),
+    ("Legal"),
+    ("Sales"),
+    ("Service");
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ("Software Engineer", 120000, 1),
+    ("Accountant", 125000, 2),
+    ("Lawyer", 190000, 3),
+    ("Salesperson", 80000, 4),
+    ("Customer Service Rep.", 60000, 5);
+    
